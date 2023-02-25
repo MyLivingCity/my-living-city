86414ca1ace0061eaaff3d39f9a9afa6bd49c3e2
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2023-02-24 17:59:27 -0800

    feature: Improved clarity of database calls.

--- server/controllers/publicProfile.js
+++ server/controllers/publicProfile.js
@@ -6,66 +6,30 @@ const prisma = require('../lib/prismaClient');
 
 const fs = require('fs');
 
-// get request to get a community business profile
-publicProfileRouter.post(
+// Get profile by userId
+publicProfileRouter.get(
     '/communityBusinessProfile/:userId',
+    passport.authenticate('jwt', { session: false }),
     async (req, res) => {
         try {
-            const {userId} = req.params;
-            console.log(userId);
+            const { userId } = req.params;
             if (!userId) {
                 return res.status(400).json({
                     message: `A valid userId must be specified in the route paramater.`,
                 });
             }
 
-            const foundUser = await prisma.user.findUnique({ where: { id: userId } });
-
-            if (!foundUser) {
-                return res.status(400).json({
-                    message: `The user with that listed ID (${userId}) does not exist.`,
-                });
-            }
-
-            const foundCommunityBusiness = await prisma.publicCommunityBusinessProfile.findUnique({
+            const result = await prisma.publicCommunityBusinessProfile.findUnique({
                 where: { userId: userId },
             });
 
-            if (!foundCommunityBusiness) {
-                const profileNew = await prisma.publicCommunityBusinessProfile.create({
-                    data: {
-                        userId: userId,
-                        statement: "",
-                        description: "",
-                        links: [],
-                        address: "",
-                        contactEmail: "",
-                        contactPhone: "",
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                        user: foundUser,
-                    }           
+            if (!result) {
+                return res.status(400).json({
+                    message: `The user with that listed ID (${userId}) does not exist.`,
                 });
-                return res.status(201).json(profileNew);
             } else {
-                let resLinks = [];
-                if (foundCommunityBusiness.links.length > 0){
-                    foundCommunityBusiness.links.forEach((link) => {
-                        resLinks.push((link.linkType + ' ' + link.link));
-                    });
-                }
-                return res.send({
-                    data: {
-                        statement: foundCommunityBusiness.statement,
-                        description: foundCommunityBusiness.description,
-                        links: resLinks,
-                        address: foundCommunityBusiness.address,
-                        contactEmail: foundCommunityBusiness.contactEmail,
-                        contactPhone: foundCommunityBusiness.contactPhone,
-                    }
-                });
+                return res.status(200).json(result);
             }
-
         } catch (error) {
             console.log(error);
             return res.status(500).json({ message: 'Internal server error' });
@@ -75,54 +39,55 @@ publicProfileRouter.post(
     }
 );
 
-// // post request to update a community business profile
+// Update profile, create if the profile does not exist.
 publicProfileRouter.put(
-    '/communityBusinessProfile',
+    '/communityBusinessProfile/:userId',
     passport.authenticate('jwt', { session: false }),
     async (req, res) => {
         try {
-            const userId = req.body.userId;
+            const { userId } = req.params;
             if (!userId) {
                 return res.status(400).json({
                     message: `A valid userId must be specified in the route paramater.`,
                 });
             }
 
-            const communityBusinessProfile = await prisma.publicCommunityBusinessProfile.findUnique({
+            const data = req.body;
+            const { statement, description, links, address, contactEmail, contactPhone } = data;
+            const updatedAt = new Date();
+
+            const result = await prisma.publicCommunityBusinessProfile.upsert({
                 where: { userId: userId },
+                update: {
+                    statement: statement,
+                    description: description,
+                    links: links,
+                    address: address,
+                    contactEmail: contactEmail,
+                    contactPhone: contactPhone,
+                    updatedAt: updatedAt,
+                },
+                create: {
+                    userId: userId,
+                    statement: statement,
+                    description: description,
+                    links: links,
+                    address: address,
+                    contactEmail: contactEmail,
+                    contactPhone: contactPhone,
+                    createdAt: updatedAt,
+                    updatedAt: updatedAt,
+                },
             });
-            
-            const links = req.body.links;
-            let linksNew = [];
-            if (links) {
-                links.forEach(async (link) => {
-                    const linkParts = link.url.split(' ');
-                    const linkNew = await prisma.link.upsert({
-                        data: {
-                            link: linkParts[1],
-                            linkType: linkParts[0],
-                            createdAt: new Date(),
-                            public_Community_Business_ProfileId: communityBusinessProfile.id,
-                        }
-                    });
-                    linksNew.push(linkNew);
+
+            if (!result) {
+                return res.status(400).json({
+                    message: `The user with that listed ID (${userId}) does not exist.`,
                 });
+            } else {
+                return res.status(200).json(result);
             }
 
-
-            const updatedProfile = await prisma.publicCommunityBusinessProfile.upsert({
-                where: { userId: userId },
-                data: {
-                    statement: req.body.statement,
-                    description: req.body.description,
-                    address: req.body.address,
-                    links: linksNew,
-                    contactEmail: req.body.contactEmail,
-                    contactPhone: req.body.contactPhone,
-                    updatedAt: new Date(),
-                }
-            });
-            return res.status(200).json(updatedProfile);
 
         } catch (error) {
             console.log(error);

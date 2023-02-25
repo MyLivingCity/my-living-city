7bfb4d50a493f6b2b8147a583c864f648b651977
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2023-02-25 15:55:27 -0800

    feature: Adjusted backend calls with authorization

--- frontend/src/lib/api/publicProfileRoutes.ts
+++ frontend/src/lib/api/publicProfileRoutes.ts
@@ -1,38 +1,41 @@
 import axios, { AxiosResponse } from "axios";
 import { API_BASE_URL, USER_TYPES } from "../constants";
 import { IUser } from "../types/data/user.type";
+import { PublicCommunityBusinessProfile } from "../types/data/publicProfile.type";
 import { getAxiosJwtRequestOption } from "./axiosRequestOptions";
 
 
-export const getCommunityBusinessProfile = async (userId: string | undefined) => {
+export const getCommunityBusinessProfile = async (
+    userId: string | undefined,
+    token: string | null
+    ) => {
     const res = await axios({
         method: "get",
         url: `${API_BASE_URL}/publicProfile/communityBusinessProfile/${userId}`,
+        headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+            "Access-Control-Allow-Origin": "*",
+        },
     });
     
     return res.data;
 }
 
 export const updateCommunityBusinessProfile = async (
-    userId: string | undefined,
-    statement: string | undefined,
-    description: string | undefined,
-    links: string[] | undefined,
-    address: string | undefined,
-    contactEmail: string | undefined,
-    contactPhone: string | undefined,
+    publicProfileData: PublicCommunityBusinessProfile,
+    token: string | null
 ) => {
     const res = await axios({
         method: "put",
-        url: `${API_BASE_URL}/communityBusinessProfile`,
-        data: {
-            statement: statement,
-            description: description,
-            links: links,
-            address: address,
-            contactEmail: contactEmail,
-            contactPhone: contactPhone,
-        }
-    })
+        url: `${API_BASE_URL}/publicProfile/communityBusinessProfile` + `/${publicProfileData.userId}`,
+        data: publicProfileData,
+        headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+            "Access-Control-Allow-Origin": "*",
+        },
+        withCredentials: true
+    });
     return res.data;
 }

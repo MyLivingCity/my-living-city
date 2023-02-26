5778edf9a0052f4ec1870026d6c909a6fb86303d
Gabriel <gabrielmclarin@gmail.com>
2023-02-26 13:49:57 -0800

    Added routes for municipal user data

--- frontend/src/lib/api/publicProfileRoutes.ts
+++ frontend/src/lib/api/publicProfileRoutes.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { API_BASE_URL, USER_TYPES } from "../constants";
 import { IUser } from "../types/data/user.type";
-import { PublicCommunityBusinessProfile } from "../types/data/publicProfile.type";
+import { PublicCommunityBusinessProfile, PublicMunicipalProfile } from "../types/data/publicProfile.type";
 import { getAxiosJwtRequestOption } from "./axiosRequestOptions";
 
 
@@ -47,6 +47,58 @@ export const getCommunityBusinessLinks = async (
     const res = await axios({
         method: "get",
         url: `${API_BASE_URL}/publicProfile/communityBusinessProfile/${profileId}/links`,
+        headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+            "Access-Control-Allow-Origin": "*",
+        },
+    });
+
+    return res.data;
+}
+
+export const getMunicipalProfile = async (
+    userId: string | undefined,
+    token: string | null
+    ) => {
+    const res = await axios({
+        method: "get",
+        url: `${API_BASE_URL}/publicProfile/municipalProfile/${userId}`,
+        headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+            "Access-Control-Allow-Origin": "*",
+        },
+    });
+
+    return res.data;
+}
+
+export const updateMunicipalProfile = async (
+    publicProfileData: PublicMunicipalProfile,
+    token: string | null
+    ) => {
+    const res = await axios({
+        method: "put",
+        url: `${API_BASE_URL}/publicProfile/municipalProfile` + `/${publicProfileData.userId}`,
+        data: publicProfileData,
+        headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+            "Access-Control-Allow-Origin": "*",
+        },
+        withCredentials: true
+    });
+    return res.data;
+}
+
+export const getMunicipalLinks = async (
+    profileId: number | undefined,
+    token: string | null
+    ) => {
+    const res = await axios({
+        method: "get",
+        url: `${API_BASE_URL}/publicProfile/municipalProfile/${profileId}/links`,
         headers: {
             "Content-Type": "application/json",
             "x-auth-token": token,

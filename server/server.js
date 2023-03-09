a9a36693929711991c3bda5f47001de4d3d894d1
RealGoldenGeneral <ncheng25@my.bcit.ca>
2023-03-09 14:28:02 -0800

    Added feedback rating route in router

--- server/server.js
+++ server/server.js
@@ -48,6 +48,7 @@ const main = async () => {
 	const imageRouter = require('./controllers/image');
 	const categoryRouter = require('./controllers/category');
 	const ideaRatingRouter = require('./controllers/rating');
+	const feedbackRatingRouter = require('./controllers/feedbackRating');
 	const commentInteractRouter = require('./controllers/commentInteract');
 	const championRouter = require('./controllers/champion');
 	const advertisementRouter = require('./controllers/advertisement');
@@ -108,6 +109,7 @@ const main = async () => {
 	apiRouter.use('/banPost', banPostRouter);
 	apiRouter.use('/banComment', banCommentRouter);
 	apiRouter.use('/publicProfile', publicProfileRouter);
+	apiRouter.use('/feedbackRating', feedbackRatingRouter);
 
 	// Listen to server
 	app.listen(PORT, console.log(`Server running on PORT:${PORT}\n\n`));

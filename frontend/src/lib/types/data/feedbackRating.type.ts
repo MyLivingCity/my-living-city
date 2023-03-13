c60af7ff66bfa6eb24f06466584e122387d14646
RealGoldenGeneral <ncheng25@my.bcit.ca>
2023-03-13 13:54:58 -0700

    Removed breakdown and counts

--- frontend/src/lib/types/data/feedbackRating.type.ts
+++ frontend/src/lib/types/data/feedbackRating.type.ts
@@ -9,18 +9,8 @@ export interface IFeedbackRating {
     updatedAt: string;
 }
 
-export interface IFeedbackRatingScaleValueBreakdown {
-    oneRatings: number;
-    twoRatings: number;
-    threeRatings: number;
-    fourRatings: number;
-    fiveRatings: number;
-}
-
 export interface IFeedbackRatingScaleAggregateSummary {
     ratingAvg: number;
-    ratingCount: number;
-    ratingValueBreakdown: IFeedbackRatingScaleValueBreakdown;
 }
 
 export interface IFeedbackRatingScaleAggregateResponse {
@@ -30,7 +20,6 @@ export interface IFeedbackRatingScaleAggregateResponse {
 
 export interface IFeedbackRatingYesNoAggregateSummary {
     ratingRatio: number;
-    ratingCount: number;
     yesRatings: number;
     noRatings: number;
 }

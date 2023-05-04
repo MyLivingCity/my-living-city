936dfba1482c8e733c3a219d3021925dd2f2ab88
DelaineWTan <112919426+DelaineWTan@users.noreply.github.com>
2023-05-03 20:04:07 -0700

    Move Post Engagement Buttons Below Idea/Proposal Titles (#97)
    * Move post engagement buttons below the title for an idea.
    
    * Move post engagement buttons below the title for a proposal.

--- frontend/src/components/content/SingleProposalPageContent.tsx
+++ frontend/src/components/content/SingleProposalPageContent.tsx
@@ -458,13 +458,13 @@ const SingleProposalPageContent: React.FC<SingleIdeaPageContentProps> = ({
         <Row>
           <Col sm={12}>
             <Card.Header>
-              <div className="d-flex justify-content-between">
+              <div className="d-flex flex-column justify-content-between">
                 <h1 className="h1">{
                   titleText.length > 75 ?
                   titleText.substring(0, 75) + "..." :
                   titleText
                 }</h1>
-                <div style={{display: "flex", minWidth: "16rem", justifyContent: "center", marginTop: "0.5rem"}}>
+                <div style={{display: "flex", minWidth: "16rem", justifyContent: "left", marginTop: "0.5rem"}}>
                   <div>
                 {showFlagButton ? (<ButtonGroup className="mr-2">
                   {!reviewed ? (

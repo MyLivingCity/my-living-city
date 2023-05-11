aa59cf7967e4d953640f58bea00b0d7bc9b84474
Oscar <oscarla5747@gmail.com>
2023-05-11 13:11:29 -0700

    Add S3 image bucket module

---
+++ server/lib/imageBucket.js
@@ -0,0 +1,13 @@
+const AWS = require("aws-sdk");
+
+async function uploadImage() {
+    // todo
+}
+
+async function accessImage() {
+    // todo
+}
+
+async function deleteImage() {
+    // todo
+}

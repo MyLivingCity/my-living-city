f4e5a01695a84ca1b80cd6fa4c6f26a0c0c1bdee
Oscar <oscarla5747@gmail.com>
2023-05-12 13:02:59 -0700

    Implement S3 image access

--- server/lib/imageBucket.js
+++ server/lib/imageBucket.js
@@ -1,13 +1,29 @@
-const AWS = require("aws-sdk");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { AWS_CONFIG, AWS_S3_BUCKET_NAME } = require("./constants");
+
+const client = new S3Client(AWS_CONFIG);
 
 async function uploadImage() {
     // todo
 }
 
-async function accessImage() {
-    // todo
+const IMG_EXPIRY_TIME = 10;
+async function accessImage(imageFolder, imageKey) {
+    const command = new GetObjectCommand({
+        Bucket: AWS_S3_BUCKET_NAME,
+        Key: `${imageFolder}/${imageKey}`,
+    });
+
+    return await getSignedUrl(client, command, { expiresIn: IMG_EXPIRY_TIME });
 }
 
 async function deleteImage() {
     // todo
 }
+
+module.exports = { 
+    uploadImage, 
+    accessImage, 
+    deleteImage 
+};

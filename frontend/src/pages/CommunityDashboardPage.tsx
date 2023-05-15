983703bfa8ff51d114c49588f256f23cc86e85d9
Ethan Lee <33e.lee@gmail.com>
2023-05-15 10:30:35 -0700

    Add handling for non array type object

--- frontend/src/pages/CommunityDashboardPage.tsx
+++ frontend/src/pages/CommunityDashboardPage.tsx
@@ -34,7 +34,12 @@ const CommunityDashboardPage: React.FC<CommunityDashboardPageProps> = (props) =>
 
     // if segId == 0 then use userSegments to set segId to the home segment
     if (parseInt(segId, 10) === 0 && userSegments) {
-        let home_segment_id = userSegments.filter((seg: any) => seg.segType === "Segment" && seg.userType == "Resident")[0].id;
+        let home_segment_id = 0
+        if (Array.isArray(userSegments)) {
+            home_segment_id = (userSegments).filter((seg: any) => seg.segType === "Segment" && seg.userType === "Resident")[0].homeSegmentId;
+        } else {
+            home_segment_id = userSegments.homeSegmentId
+        }
 
         props.history.push(`/community-dashboard/${home_segment_id}`);
         // segId = home_segment_id.toString();

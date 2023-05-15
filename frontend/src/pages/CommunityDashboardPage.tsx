1df9dc09cedc4ee57546cc79bb46c03b2530762d
Ethan Lee <33e.lee@gmail.com>
2023-05-15 09:56:37 -0700

    Fix logic error

--- frontend/src/pages/CommunityDashboardPage.tsx
+++ frontend/src/pages/CommunityDashboardPage.tsx
@@ -33,8 +33,9 @@ const CommunityDashboardPage: React.FC<CommunityDashboardPageProps> = (props) =>
     } = useAllUserSegments(token, user?.id || null);
 
     // if segId == 0 then use userSegments to set segId to the home segment
-    if (parseInt(segId) === 0 && userSegments) {
+    if (parseInt(segId, 10) === 0 && userSegments) {
         let home_segment_id = (userSegments || []).filter((seg: any) => seg.segType === "Segment" && seg.userType == "Resident")[0].id;
+
         console.log("home_segment_id: ", home_segment_id);
         props.history.push(`/community-dashboard/${home_segment_id}`);
         // segId = home_segment_id.toString();

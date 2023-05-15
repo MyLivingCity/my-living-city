c95332e9f4406001b62fba1d20080731a5491d4a
kazma-ku <77481133+kazma-ku@users.noreply.github.com>
2023-05-15 10:12:59 -0700

    R-200d: Create and implement module for handling image upload from AWS

--- server/lib/imageBucket.js
+++ server/lib/imageBucket.js
@@ -1,11 +1,44 @@
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { AWS_CONFIG, AWS_S3_BUCKET_NAME } = require("./constants");
-
 const client = new S3Client(AWS_CONFIG);
 
-async function uploadImage() {
-    // todo
+//required modules for uploading image to s3 bucket
+const AWS = require("aws-sdk");
+const e = require("express");
+const fs = require("fs");
+
+async function uploadImage(folderName, fileName, fileContent) {
+    // the AWS configuration
+    AWS.config.update({
+        accessKeyId: "AKIAZ43AZF65ZWKZNK4U",
+        secretAccessKey:"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",
+        region: "us-west-2",
+    })
+
+    // the file to be uploaded
+    const filePath = "../uploads/$(folderName)/$(fileName)"
+    const fileContent = fs.readFileSync(filePath);
+
+    // the parameters for uploading
+    const s3 = new AWS.S3();
+    const bucketName = "image-mylivingcity";
+    const key = $(folderName)/$(fileName);
+    const content = fileContent;
+
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: content,
+    };
+
+    // uploading the file
+    try{
+        const response = await s3.upload(params).promise();
+        console.log("Uploaded Successully", response);
+    }catch(error){
+        console.error("Uploading failed", error.message);
+    }
 }
 
 const IMG_EXPIRY_TIME = 60; // in seconds

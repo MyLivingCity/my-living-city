943b9c4367c7f106419f62cb16a7eba4841b63cc
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2023-05-15 09:59:46 -0700

    R-002e: Implement and apply AWS S3 image access (#120)
    * Add s3 presigner and client packages
    
    * Add AWS config constants
    
    * Implement S3 image access
    
    * Add AWS image request to idea routes
    
    * Raise expiry time to 60 seconds
    
    * Add util function to process multiple image paths
    
    * Add comments to access image implementation
    
    * Bugfix for object initialization
    
    * Add to advertisement and user paths
    
    * Add import and convert unique users to lists

--- server/lib/imageBucket.js
+++ server/lib/imageBucket.js
@@ -1,13 +1,37 @@
-const AWS = require("aws-sdk");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { AWS_CONFIG, AWS_S3_BUCKET_NAME } = require("./constants");
+
+const client = new S3Client(AWS_CONFIG);
 
 async function uploadImage() {
     // todo
 }
 
-async function accessImage() {
-    // todo
+const IMG_EXPIRY_TIME = 60; // in seconds
+/**
+ * Requests a signed URL for an image from the AWS S3 bucket. The URL expires
+ * within a specified amonut of time.
+ * 
+ * @param { string } imageFolder    The folder path the image resides in
+ * @param { string } imageKey       The name of the image
+ * @returns { string }              The pre-signed url
+ */
+async function accessImage(imageFolder, imageKey) {
+    const command = new GetObjectCommand({
+        Bucket: AWS_S3_BUCKET_NAME,
+        Key: `${imageFolder}/${imageKey}`,
+    });
+
+    return await getSignedUrl(client, command, { expiresIn: IMG_EXPIRY_TIME });
 }
 
 async function deleteImage() {
     // todo
 }
+
+module.exports = { 
+    uploadImage, 
+    accessImage, 
+    deleteImage 
+};

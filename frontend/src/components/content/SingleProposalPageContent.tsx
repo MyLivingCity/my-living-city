48360becc35325761382f1ffec50514cebf42552
Ethan <71413330+ethanlee123@users.noreply.github.com>
2023-05-17 16:07:42 -0700

    Proposal post to be limited to the same segment as the proposal (#130)
    * Pass municipality via query params
    
    * Show single municipaity when proposing idea
    
    * Rename vars and remove unused import

--- frontend/src/components/content/SingleProposalPageContent.tsx
+++ frontend/src/components/content/SingleProposalPageContent.tsx
@@ -68,6 +68,10 @@ interface SingleIdeaPageContentProps {
   ideaId: string;
 }
 
+const getSegmentName = (segment: string | undefined): string => {
+  return segment ? capitalizeFirstLetterEachWord(segment) : "N/A";
+}
+
 const SingleProposalPageContent: React.FC<SingleIdeaPageContentProps> = ({
   ideaData,
   proposalData,
@@ -150,8 +154,17 @@ const SingleProposalPageContent: React.FC<SingleIdeaPageContentProps> = ({
     return !ideaData.champion && !!ideaData.isChampionable;
   };
 
+
   function redirectToIdeaSubmit() {
-    window.location.href = `/submit?supportedProposal=${proposalId}`;
+    let name = segment?.name
+    
+    if (!name && superSegment) {
+      name = superSegment.name
+    }
+
+    const communityOfInterest = getSegmentName(name);
+
+    window.location.href = `/submit?supportedProposal=${proposalId}&communityOfInterest=${communityOfInterest}`;
   }
 
   const { token, user } = useContext(UserProfileContext);
@@ -556,9 +569,7 @@ const SingleProposalPageContent: React.FC<SingleIdeaPageContentProps> = ({
                   {segment ? (
                     <h4 className="h5">
                       Municipality:{" "}
-                      {segment
-                        ? capitalizeFirstLetterEachWord(segment.name)
-                        : "N/A"}
+                      {getSegmentName(segment.name)}
                     </h4>
                   ) : null}
                   {subSegment ? (

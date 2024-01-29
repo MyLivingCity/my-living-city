ad81e9aa8d15074e38a5dd08b0144818045c7eef
Hairun Huang <hairunhuang@gmail.com>
2024-01-29 15:35:02 -0800

    commit again

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,7 +1,9 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const API_BASE_URL =
   process.env.REACT_APP_BASE_API_URL || "http://localhost:3001";
-export const MAP_KEY = String(process.env.REACT_APP_GOOGLE_PHYSICAL_MAP_API_KEY);
+export const MAP_KEY = String(
+  process.env.REACT_APP_GOOGLE_PHYSICAL_MAP_API_KEY
+);
 // UTILITY defaults
 export const UTIL_FUNCTIONS = {
   delayDefault: Number(process.env.REACT_APP_UTIL_FUNC_DELAY ?? 2000),
@@ -56,6 +58,7 @@ export const ROUTES = {
   SINGLE_PROPOSAL: "/proposals/:proposalId",
   SUBMIT_IDEA: "/submit",
   LOGIN: "/login",
+  CHECKEMAIL: "/check-email",
   REGISTER: "/register",
   USER_PROFILE: "/profile",
   TEST_PAGE: "/test",
---
+++ frontend/src/pages/CheckEmailPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CheckEmailPageContent from "src/components/content/CheckEmailPageContent";
+
+function CheckEmailPage() {
+  return (
+    <div>
+      <CheckEmailPageContent />
+    </div>
+  );
+}
+
+export default CheckEmailPage;

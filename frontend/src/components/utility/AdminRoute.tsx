e53ba9e3cd1b3babe02d62dec62ef6e56f7b3eb6
Stefan Chen <zchen158@my.bcit.ca>
2024-03-20 21:23:20 -0700

    f

--- frontend/src/components/utility/AdminRoute.tsx
+++ frontend/src/components/utility/AdminRoute.tsx
@@ -4,40 +4,43 @@ import { UserProfileContext } from '../../contexts/UserProfile.Context';
 import { ROUTES } from '../../lib/constants';
 
 /**
- * Private Route Higher Order Component that checks if the user is logged in
+ * Private Route Higher Order Component that checks if the user is logged in 
  * and redirects them to the desired authenticated page or redirects them to login page
- * if they are not authenticated.
+ * if they are not authenticated. 
  */
 interface AdminRouteProps {
-     redirectPath?: string;
-     path: RouteProps['path'];
-     component: React.ElementType;
-}
+    redirectPath?: string;
+    path: RouteProps['path'];
+    component: React.ElementType
+    }
 
 const AdminRoute: React.FC<AdminRouteProps> = ({
-     redirectPath,
-     component: Component,
-     ...routeProps
+    redirectPath,
+    component: Component,
+    ...routeProps
 }) => {
-     const { user, token } = useContext(UserProfileContext);
-     const ComponentToRender = Component as React.ElementType;
-     let isLoggedIn = false;
-     if (!!user && !!token) {
-          isLoggedIn = true;
-     }
+    const {
+        user,
+        token
+    } = useContext(UserProfileContext);
+    const ComponentToRender = Component as React.ElementType;
+    let isLoggedIn = false;
+    if (!!user && !!token) {
+        isLoggedIn = true;
+    }
 
-     return (
-          <Route
-              {...routeProps}
-              render={(props) =>
-                  isLoggedIn && (user!.userType === 'SUPER_ADMIN' || user!.userType === 'ADMIN') ? (
-                      <ComponentToRender {...props} />
-                  ) : (
-                      <Redirect to={ redirectPath || ROUTES.LOGIN } />
-                  )
-              }
-          />
-      );
+    return (
+        <Route
+            {...routeProps}
+            render={(props) =>
+                isLoggedIn && (user!.userType === 'SUPER_ADMIN' || user!.userType === 'ADMIN') ? (
+                    <ComponentToRender {...props} />
+                ) : (
+                    <Redirect to={ redirectPath || ROUTES.LOGIN } />
+                )
+            }
+        />
+    );
 };
 
 export default AdminRoute;

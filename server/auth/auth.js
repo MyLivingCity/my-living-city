9c42e31efab83a3dfdff4857a695b80ea6024b87
Harrison de Jong <harrisondijon@gmail.com>
2024-11-09 22:35:21 -0800

    Removed old code from server\auth\auth.js. Added a more comprehensive email for verfication from the original verification

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -7,7 +7,6 @@
   const nodemailer = require('nodemailer');
   const { getSegmentInfo } = require('../helpers/userSegmentHelpers');
   const { google } = require('googleapis');
-  const OAuth2 = google.auth.OAuth2;
 
   passport.use(
     "signup",
@@ -92,7 +91,6 @@
           if (userExists && userExists.verified !== true) {
             // Send email verification
             sendEmailVerification(userExists)
-            .then((result) => console.log("Email sent...", result))
             .catch((error) => console.log(error.message));
             
             return done(null, false, { message: "User already exists. Please check your email for verification link."});
@@ -357,70 +355,6 @@
     )
   )
 
-  // const sendEmailVerification = async (user) => {
-  //   // Set up OAuth2 client with credentials from environment variables
-  //   const oauth2Client = new OAuth2(
-  //     process.env.GOOGLE_CLIENT_ID,
-  //     process.env.GOOGLE_CLIENT_SECRET,
-  //     "http://localhost:3000/auth/callback"
-  //   );
-
-  //   oauth2Client.setCredentials({
-  //     refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-  //   });
-
-  //   // Get a fresh access token
-  //   const accessToken = await oauth2Client.getAccessToken();
-
-  //   if (accessToken.token) {
-  //     console.log("Access Token successfully generated:", accessToken.token);
-  //   } else {
-  //     console.error("Failed to generate Access Token:", accessToken);
-  //   }
-
-  //   // Configure Nodemailer with OAuth2 for Gmail
-  //   const transporter = nodemailer.createTransport({
-  //     service: 'gmail',
-  //     auth: {
-  //       type: 'OAuth2',
-  //       user: process.env.EMAIL,
-  //       clientId: process.env.GOOGLE_CLIENT_ID,
-  //       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  //       refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  //       accessToken: accessToken.token,  // Ensure the token is used here
-  //     },
-  //   });
-
-  //   // Generate a unique verification token for the user
-  //   let token = Math.random().toString(36).substring(2, 8).toUpperCase();
-  //   await prisma.user.update({
-  //     where: { id: user.id },
-  //     data: { verifiedToken: token },
-  //   });
-
-  //   // Create the verification link
-  //   const appUrl = process.env.APP_URL || 'http://localhost:3000';
-  //   const url = `${appUrl}/emailVerification/checkVerificationCode/${user.id}/${token}`;
-
-  //   const mailOptions = {
-  //     from: `MyLivingCity Email Verification <${process.env.EMAIL}>`,
-  //     to: user.email,
-  //     subject: "Email Verification",
-  //     text: `Please click the link to verify your email: ${url}`,
-  //     html: `<p>Tap the button below to confirm your email address. If you didn't create an account with <a href="${appUrl}">MyLivingCity</a>, you can safely delete this email.</p>
-  //           <p><a href="${url}" style="display: inline-block; padding: 16px 36px; background-color: #1a82e2; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a></p>`
-  //   };
-
-  //   // Send email and handle errors
-  //   try {
-  //     await transporter.sendMail(mailOptions);
-  //     console.log("Verification email sent successfully!");
-  //   } catch (error) {
-  //     console.error("Error sending verification email:", error);
-  //   }
-  // };
-
-
   const sendEmailVerification = async (user) => {
     const oAuth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
@@ -445,26 +379,273 @@
             },
       });
 
-      console.log(user.email);
+      // Generate a unique verification token for the user
+      let token = Math.random().toString(36).substring(2, 8).toUpperCase();
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { verifiedToken: token },
+      });
+
+      console.log(token);
 
       const appUrl = process.env.APP_URL || 'http://localhost:3000';
+
+      //required for testing:
+      // var url = 'http://localhost:3001';
+
       var url = process.env.APP_URL || 'http://localhost:3001';
+      url += `/emailVerification/checkVerificationCode/${user.id}/${token}`;
 
       const mailOptions = {
-            from: `MyLivingCity Email Verification <${process.env.EMAIL}>`,
-            to: user.email,
-            subject: "Email Verification",
-            text: `Please click the link to verify your email: ${url}`,
-            html: `<p>Tap the button below to confirm your email address. If you didn't create an account with <a href="${appUrl}">MyLivingCity</a>, you can safely delete this email.</p>
-                  <p><a href="${url}" style="display: inline-block; padding: 16px 36px; background-color: #1a82e2; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a></p>`
+        from: 'MyLivingCity Email Verification<' + process.env.EMAIL + '>', // sender address
+        to: user.email, // list of receivers
+        subject: "Email Verification", // Subject line
+        text: "Please click the link to verify your email" + url, // plain text body
+        // Html should have an element with a get request to the url
+        // html: `<a href="${url}">Click here to verify your email</a>
+        // email template: https://codepen.io/md-khokon/pen/bPLqzV
+        html: `<!DOCTYPE html>
+        <html>
+        <head>
+    
+          <meta charset="utf-8">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <title>Email Confirmation</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <style type="text/css">
+          /**
+           * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+           */
+          @media screen {
+            @font-face {
+              font-family: 'Source Sans Pro';
+              font-style: normal;
+              font-weight: 400;
+              src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+            }
+            @font-face {
+              font-family: 'Source Sans Pro';
+              font-style: normal;
+              font-weight: 700;
+              src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+            }
+          }
+          /**
+           * Avoid browser level font resizing.
+           * 1. Windows Mobile
+           * 2. iOS / OSX
+           */
+          body,
+          table,
+          td,
+          a {
+            -ms-text-size-adjust: 100%; /* 1 */
+            -webkit-text-size-adjust: 100%; /* 2 */
+          }
+          /**
+           * Remove extra space added to tables and cells in Outlook.
+           */
+          table,
+          td {
+            mso-table-rspace: 0pt;
+            mso-table-lspace: 0pt;
+          }
+          /**
+           * Better fluid images in Internet Explorer.
+           */
+          img {
+            -ms-interpolation-mode: bicubic;
+          }
+          /**
+           * Remove blue links for iOS devices.
+           */
+          a[x-apple-data-detectors] {
+            font-family: inherit !important;
+            font-size: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            color: inherit !important;
+            text-decoration: none !important;
+          }
+          /**
+           * Fix centering issues in Android 4.4.
+           */
+          div[style*="margin: 16px 0;"] {
+            margin: 0 !important;
+          }
+          body {
+            width: 100% !important;
+            height: 100% !important;
+            padding: 0 !important;
+            margin: 0 !important;
+          }
+          /**
+           * Collapse table borders to avoid space between cells.
+           */
+          table {
+            border-collapse: collapse !important;
+          }
+          a {
+            color: #1a82e2;
+          }
+          img {
+            height: auto;
+            line-height: 100%;
+            text-decoration: none;
+            border: 0;
+            outline: none;
+          }
+          </style>
+    
+        </head>
+        <body style="background-color: #e9ecef;">
+    
+          <!-- start preheader -->
+          <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+            MyLivingCity Email Verification.
+          </div>
+          <!-- end preheader -->
+    
+          <!-- start body -->
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    
+            <!-- start logo -->
+            <tr>
+              <td align="center" bgcolor="#e9ecef">
+                <!--[if (gte mso 9)|(IE)]>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+                <tr>
+                <td align="center" valign="top" width="600">
+                <![endif]-->
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                  <tr>
+                    <td align="center" valign="top" style="padding: 36px 24px;">
+                      <a href=${appUrl} target="_blank" style="display: inline-block;">
+                        <img src="https://raw.githubusercontent.com/MyLivingCity/my-living-city/development/frontend/public/logo192x192.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                      </a>
+                    </td>
+                  </tr>
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+              </td>
+            </tr>
+            <!-- end logo -->
+    
+            <!-- start hero -->
+            <tr>
+              <td align="center" bgcolor="#e9ecef">
+                <!--[if (gte mso 9)|(IE)]>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+                <tr>
+                <td align="center" valign="top" width="600">
+                <![endif]-->
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                  <tr>
+                    <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+                      <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
+                    </td>
+                  </tr>
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+              </td>
+            </tr>
+            <!-- end hero -->
+    
+            <!-- start copy block -->
+            <tr>
+              <td align="center" bgcolor="#e9ecef">
+                <!--[if (gte mso 9)|(IE)]>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+                <tr>
+                <td align="center" valign="top" width="600">
+                <![endif]-->
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+    
+                  <!-- start copy -->
+                  <tr>
+                    <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                      <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with <a href=${appUrl}>MyLivingCity</a>, you can safely delete this email.</p>
+                    </td>
+                  </tr>
+                  <!-- end copy -->
+    
+                  <!-- start button -->
+                  <tr>
+                    <td align="left" bgcolor="#ffffff">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                          <td align="center" bgcolor="#ffffff" style="padding: 12px;">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                              <tr>
+                                <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
+                                  <a href=${url} target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <!-- end button -->
+    
+                  <!-- start copy -->
+                  <tr>
+                    <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
+                      <p style="margin: 0;">Thanks,<br> MyLivingCity</p>
+                    </td>
+                  </tr>
+                  <!-- end copy -->
+    
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+              </td>
+            </tr>
+            <!-- end copy block -->
+    
+            <!-- start footer -->
+            <tr>
+              <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+                <!--[if (gte mso 9)|(IE)]>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+                <tr>
+                <td align="center" valign="top" width="600">
+                <![endif]-->
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                </table>
+                <!--[if (gte mso 9)|(IE)]>
+                </td>
+                </tr>
+                </table>
+                <![endif]-->
+              </td>
+            </tr>
+            <!-- end footer -->
+    
+          </table>
+          <!-- end body -->
+    
+        </body>
+        </html>`
+    
       };
 
-      const result = transport.sendMail(mailOptions);
-      return result;
-          
-    } catch (error) {
-      console.error("Error sending verification email:", error);
-    }
-  
-  }
+      await transport.sendMail(mailOptions);
 
+    } catch (error) {
+        console.error('Error sending verification email:', error);
+        throw new Error('Failed to send verification email.');
+    }
+  };

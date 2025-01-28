5ed4063bb905c0d6cdf761afc50e6cd493591f55
Radmir Garipov <radmirgaripov@Radmirs-MacBook-Air.local>
2025-01-28 13:43:11 -0800

    Initial commit

--- server/controllers/schoolDetails.js
+++ server/controllers/schoolDetails.js
@@ -157,25 +157,6 @@ schoolDetailsRouter.patch(
     '/updateCityNeighbourhood/:id',
     async (req, res) => {
         try {
-            if (req.body.neighbourhood === '') {
-                const result = await prisma.userSegments.update({
-                    where: {
-                        userId: req.params.id,
-                    },
-                    data: {
-                        schoolSubSegmentId: null,
-                        schoolSubSegmentName: '',
-                    },
-                });
-
-                res.status(200).json({
-                    message: 'City and neighbourhood updated successfully',
-                    result,
-                });
-
-                return;
-            }
-
             const city = await prisma.segments.findFirst({
                 where: { name: { equals: req.body.city, mode: 'insensitive' } },
             });

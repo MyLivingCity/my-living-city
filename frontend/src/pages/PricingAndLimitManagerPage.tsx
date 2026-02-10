413fd91ac9754018b662f9c769569cad8104d62e
Melissa Shao <mshao8@my.bcit.ca>
2026-02-09 20:34:01 -0800

    C2 scope: Pricing and Limit Manager page init

---
+++ frontend/src/pages/PricingAndLimitManagerPage.tsx
@@ -0,0 +1,8 @@
+export default function PricingAndLimitManagerPage() {
+    return (
+        <div>
+            <h1>Pricing and Limit Manager</h1>
+            <p>TODO: build pricing + limit settings UI</p>
+        </div>
+    );
+}

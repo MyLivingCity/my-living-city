4ca6b4f507f01c4ff33b8575f67ab86c75f48ac6
Chunki Lam <clam193@my.bcit.ca>
2026-02-11 15:09:15 -0800

    BUG013 format for business/community account should only show Name@SegmentName

--- frontend/src/lib/types/data/user.type.ts
+++ frontend/src/lib/types/data/user.type.ts
@@ -35,6 +35,7 @@ export interface IUser {
   school_geo?: IGeo;
   address?: IAddress;
   userRole?: IUserRole;
+  userSegment?: IUserSegment[];
   userSegments?: IUserSegment[];
   userHandles?: IUserHandle[];
   IdeaComments?: IComment[];

63e93fc749930460f4c7010d7cdade363c627da9
Cai Chun Yan <caichunyan17@gmail.com>
2026-03-18 15:58:37 -0700

    add proposal limit manager component to page

--- frontend/src/pages/PricingAndLimitManagerPage.tsx
+++ frontend/src/pages/PricingAndLimitManagerPage.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import AccountPricingContent from '../components/content/AccountPricingContent';
+import ProposalLimitManager from 'src/components/content/ProposalLimitManager';
 
 export default function PricingAndLimitManagerPage() {
     return (
@@ -9,6 +10,9 @@ export default function PricingAndLimitManagerPage() {
 
                 {/* Scope - C2-004 */}
                 <AccountPricingContent />
+
+                {/* Proposal Limit */}
+                <ProposalLimitManager />
             </Container>  
         </div>
         

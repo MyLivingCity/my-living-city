f7305cd371b658323f406a72b38553f6fcb758f7
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-19 13:39:58 -0700

    chore: fix project-wide eslint config;
    temp: working commit 2;

---
+++ eslint.config.ts
@@ -0,0 +1,50 @@
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const configGlobal = [
+  {
+    ignores: [
+      "**/dist/",
+      "server/",
+      "frontend/"
+    ]
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+];
+
+const configAppsBackend = [
+  {
+    files: ["apps/backend/**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node }
+  },
+];
+
+const configAppsFrontend = [
+  globalIgnores(["dist"]),
+  {
+    files: ["apps/frontend/**/*.{ts,tsx}"],
+    extends: [
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+]
+
+export default defineConfig([
+  ...configGlobal,
+  ...configAppsBackend,
+  ...configAppsFrontend,
+]);
--- packages/config/eslint/eslint.config.mts
+++
@@ -1,3 +0,0 @@
-import eslintConfig from "../eslint.config.mts"
-
-export default eslintConfig;

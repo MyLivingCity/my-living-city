677aa2abe09cd27fd199a2dbc96fd518ded1ab32
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-19 18:12:17 -0700

    style: formatting;

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
-    tsconfigPaths: true
+    tsconfigPaths: true,
   },
   server: {
     fs: {
@@ -18,9 +15,9 @@ export default defineConfig({
     watch: {
       usePolling: true,
       interval: 150,
-    }
+    },
   },
   optimizeDeps: {
-    exclude: ['@mlc/lib'],
+    exclude: ["@mlc/lib"],
   },
-})
+});

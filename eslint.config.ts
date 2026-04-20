3fed5ba7c1c680a1699a89919e290e5455859e80
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-19 18:09:53 -0700

    chore: setup prettier with eslint;

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,14 +4,11 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const configGlobal = [
   {
-    ignores: [
-      "**/dist/",
-      "server/",
-      "frontend/"
-    ]
+    ignores: ["**/dist/", "server/", "frontend/"],
   },
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}"],
@@ -19,12 +16,13 @@ const configGlobal = [
     extends: ["js/recommended"],
   },
   tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
 
 const configAppsBackend = [
   {
     files: ["apps/backend/**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.node }
+    languageOptions: { globals: globals.node },
   },
 ];
 
@@ -32,16 +30,13 @@ const configAppsFrontend = [
   globalIgnores(["dist"]),
   {
     files: ["apps/frontend/**/*.{ts,tsx}"],
-    extends: [
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-]
+];
 
 export default defineConfig([
   ...configGlobal,

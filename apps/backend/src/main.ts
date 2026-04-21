8a54f353af9c53e04e66c1c8a42ab6ceb5af41fc
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-21 14:33:16 -0700

    chore(backend-users): implement /me endpoint;

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { initServer } from "@ts-rest/express";
-import { createExpressEndpoints } from "@ts-rest/express";
-import { pokemonContract } from "@mlc/lib/api";
+import userApiHandlers from "./server/features/users/handler";
+import { addEndpoints } from "./server";
 import { strategyJwt } from "./lib/auth/strategy";
 import passport from "passport";
 
@@ -17,28 +16,7 @@ app.options("*", cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const s = initServer();
-
-const router = s.router(pokemonContract, {
-  getPokemon: async ({ params: { id } }) => {
-    // Mock pokemon data
-    const pokemon = { name: "Pikachu" };
-
-    if (id !== "1") {
-      return {
-        status: 404,
-        body: null,
-      };
-    }
-
-    return {
-      status: 200,
-      body: pokemon,
-    };
-  },
-});
-
-createExpressEndpoints(pokemonContract, router, app);
+addEndpoints(app, userApiHandlers);
 
 const port = process.env["port"] || 3001;
 app.listen(port, () => {

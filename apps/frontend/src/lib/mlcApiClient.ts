664e351fa9cb2756d612fc262d2d6c68aff433e6
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-22 18:16:18 -0700

    chore(frontend-lib): add ts-rest client;

---
+++ apps/frontend/src/lib/mlcApiClient.ts
@@ -0,0 +1,6 @@
+import { allApiContracts } from "@mlc/lib/api";
+import { initClient } from "@ts-rest/core";
+
+export const mlcApiClient = initClient(allApiContracts, {
+  baseUrl: "http://localhost:3001", // TODO: Implement env
+});

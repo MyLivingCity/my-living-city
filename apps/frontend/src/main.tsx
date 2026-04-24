9ab0bc1067f23fe89c596d166a2e315b94dc4488
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-24 12:33:15 -0700

    chore(frontend): import bootstrap styles from old frontend;

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ add this
+
+import "./styles/base.scss";
 import "./index.css";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(

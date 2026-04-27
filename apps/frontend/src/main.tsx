1680aaba78a8d25d58bd2dd057223fbb237aad0c
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-04-27 10:58:46 -0700

    chore(frontend): fix main.tsx regression;

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,31 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client"; // ✅ important change
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ add this
-
-import "./styles/base.scss";
-import "./index.css";
-
-import App from "./App.tsx";
->>>>>>> refactor
-
-import "@/styles/ui/_other.scss";
-
 import { QueryClientProvider } from "react-query";
 import UserProfileProvider from "src/providers/UserProfileProvider";
 import { queryClient } from "src/lib/clientInitializer";
+import App from "./App.tsx";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+import "./styles/base.scss";
+import "./index.css";
+import "@/styles/ui/_other.scss";
 
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserProfileProvider>
         <BrowserRouter>
@@ -33,5 +19,5 @@ root.render(
         </BrowserRouter>
       </UserProfileProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );

b2fc0ce5633ebe6e5a73d13b174c4b7bb2fb41bd
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-05-01 14:24:22 -0700

    chore(backend): impl. pino custom logging;

---
+++ apps/backend/src/logger.ts
@@ -0,0 +1,74 @@
+import { randomUUID } from "node:crypto";
+import pino from "pino";
+import type { Options } from "pino-http";
+
+export const log = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+export const configDefaultHttpLogger: Options = {
+  // Reuse an existing logger instance
+  logger: log,
+
+  // Define a custom request id function
+  genReqId: function (req, res) {
+    const existingID = req.id ?? req.headers["x-request-id"];
+    if (existingID) return existingID;
+    const id = randomUUID();
+    res.setHeader("X-Request-Id", id);
+    return id;
+  },
+
+  // Define custom serializers
+  serializers: {
+    err: pino.stdSerializers.err,
+    req: pino.stdSerializers.req,
+    res: pino.stdSerializers.res,
+  },
+
+  // Set to `false` to prevent standard serializers from being wrapped.
+  wrapSerializers: true,
+
+  // Define a custom logger level
+  customLogLevel: function (_, res, err: unknown) {
+    if (res.statusCode >= 400 && res.statusCode < 500) {
+      return "warn";
+    } else if (res.statusCode >= 500 || err) {
+      return "error";
+    } else if (res.statusCode >= 300 && res.statusCode < 400) {
+      return "silent";
+    }
+    return "info";
+  },
+
+  // Define a custom success message
+  customSuccessMessage: function (req, res) {
+    if (res.statusCode === 404) {
+      return "resource not found";
+    }
+    return `${req.method} completed`;
+  },
+
+  // Define a custom receive message
+  customReceivedMessage: function (req) {
+    return "request received: " + req.method;
+  },
+
+  // Define a custom error message
+  customErrorMessage: function (_, res) {
+    return "request errored with status code: " + res.statusCode;
+  },
+
+  // Override attribute keys for the log object
+  customAttributeKeys: {
+    req: "request",
+    res: "response",
+    err: "error",
+    responseTime: "timeTaken",
+  },
+};

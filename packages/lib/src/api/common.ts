066339d96301316949815952172eb685d9f45416
Geoff Warner <231114443+geoff-warner@users.noreply.github.com>
2026-05-04 14:50:32 -0700

    feat(contracts/common): simplify DecimalLikeSchema; add Schemas for lat, lon, radius

--- packages/lib/src/api/common.ts
+++ packages/lib/src/api/common.ts
@@ -1,6 +1,5 @@
 import { Decimal } from "@prisma/client/runtime/client";
 import z from "zod";
-import { Prisma } from "../../../../apps/backend/node_modules/.prisma/client/client";
 
 export const DateTimeString = z.date();
 
@@ -27,3 +26,47 @@ export const ErrorResponseSchema = z.object({
     errorStack: z.string(),
   }),
 });
+
+/**
+ * Recommended maximum precision is 6 decimal places (~10cm in GPS),
+ * which is well covered by standard JavaScript Number type (>=15)
+ * Context: commercial GPS is accurate to ~3m
+ */
+export const LatSchema = z.coerce
+  .number()
+  .min(-90, { message: "Latitude must be between -90 and 90" })
+  .max(90, { message: "Latitude must be between -90 and 90" });
+/**
+ * Recommended maximum precision is 6 decimal places (~10cm in GPS),
+ * which is well covered by standard JavaScript Number type (>=15)
+ * Context: commercial GPS is accurate to ~3m
+ */
+export const LonSchema = z.coerce
+  .number()
+  .min(-180, { message: "Longitude must be between -180 and 180" })
+  .max(180, { message: "Longitude must be between -180 and 180" });
+
+/**
+ * database has this typed as a Decimal, which is unnecessary
+ */
+export const RadiusSchema = z.coerce
+  .number()
+  .min(1, { message: "Minimum radius is 1km" })
+  //arbitrary, but should be capped to prevent abuse
+  .max(100, { message: "Maximum radius is 100km" });
+
+/**
+ * Formats to Number with recommended maximum precision of 6 decimal places.
+ * Meant for items being pulled from Decimal fields in database, until
+ * the schema can be updated to type lat, lon and radius as Float
+ */
+export const formatCoord = (val: unknown) => Number(Number(val).toFixed(6));
+
+/* Example - validate latitude entry
+const result = LatSchema.safeParse(120); // Invalid latitude
+if (!result.success) {
+  // result.error.errors is an array of error objects
+  console.log(result.error.errors[0]!.message);
+  // Output: "Latitude must be between -90 and 90"
+}
+ */

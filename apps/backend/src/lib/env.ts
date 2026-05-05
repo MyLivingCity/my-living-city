253f450eb3a7ac381b97f50b252912d36eaae420
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-05-05 12:17:11 -0700

    chore(backend): add AWS env vars to env;

--- apps/backend/src/lib/env.ts
+++ apps/backend/src/lib/env.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 
 type Env = {
+  AWS_ACCESS_KEY: string;
+  AWS_REGION: string;
+  AWS_S3_BUCKET_NAME: string;
+  AWS_SECRET_KEY: string;
   DATABASE_URL: string;
   DEEPINFRA_API_KEY: string;
   GOOGLE_MAP_API_KEY: string;
@@ -17,6 +21,10 @@ type Env = {
 };
 
 export const env: Env = {
+  AWS_ACCESS_KEY: process.env["AWS_ACCESS_KEY"]!,
+  AWS_REGION: process.env["AWS_REGION"]!,
+  AWS_S3_BUCKET_NAME: process.env["AWS_S3_BUCKET_NAME"]!,
+  AWS_SECRET_KEY: process.env["AWS_SECRET_KEY"]!,
   DATABASE_URL: process.env["DATABASE_URL"]!,
   DEEPINFRA_API_KEY: process.env["DEEPINFRA_API_KEY"]!,
   GOOGLE_MAP_API_KEY: process.env["GOOGLE_MAP_API_KEY"]!,
